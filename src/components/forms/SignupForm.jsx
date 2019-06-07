640bd189e0935c9afa01645dae2a149c605c53ec
vinhtn-1790 <tran.ngoc.vinh@sun-asterisk.com>
2019-06-07 15:10:17 +0700

    index-page

---
+++ src/components/forms/SignupForm.jsx
@@ -0,0 +1,43 @@
+// @flow
+import React from 'react';
+import '../../styles/signup-login.css';
+
+const SignupForm = () => (
+  <div className="main">
+		<section className="signup">
+				<div className="container">
+					<div className="signup-content">
+						<form id="signup-form" className="signup-form">
+							<h2 className="form-title">Create account</h2>
+							<div className="form-group">
+									<input type="text" className="form-input" name="name" id="name" placeholder="Your Name"/>
+							</div>
+							<div className="form-group">
+									<input type="email" className="form-input" name="email" id="email" placeholder="Your Email"/>
+							</div>
+							<div className="form-group">
+									<input type="password" className="form-input" name="password" id="password" placeholder="Password"/>
+									<span toggle="#password" className="zmdi zmdi-eye field-icon toggle-password"></span>
+							</div>
+							<div className="form-group">
+									<input type="password" className="form-input" name="re_password" id="re_password" placeholder="Repeat your password"/>
+							</div>
+							<div className="form-group">
+									<input type="checkbox" name="agree-term" id="agree-term" className="agree-term" />
+									<label htmlFor="agree-term" className="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#0" className="term-service">Terms of service</a></label>
+							</div>
+							<div className="form-group">
+									<input type="submit" name="submit" id="submit" className="form-submit" value="Sign up"/>
+							</div>
+						</form>
+						<p className="loginhere">
+							Have already an account ? <a href="#0" className="loginhere-link">Login here</a>
+						</p>
+					</div>
+				</div>
+		</section>
+  </div>
+);
+
+export default SignupForm;
+
